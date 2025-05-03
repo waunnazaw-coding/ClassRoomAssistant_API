3cc605787bdf27e125b07c0156fe836d7267d432
waunnazaw-coding <yeminhtet3118@gmail.com>
2025-05-04 00:55:08 +0630

    feat : add new model for stored procedure

---
+++ Models/UserNotificationRawDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassRoomClone_App.Server.Models;
+
+public class UserNotificationRawDto
+{
+    public int Id { get; set; }
+    public string Type { get; set; } = null!;
+    public int ReferenceId { get; set; }
+    public bool IsRead { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public string? AnnouncementTitle { get; set; }
+    public string? MessageContent { get; set; }
+    public string? AssignmentTitle { get; set; }
+    public string? MaterialTitle { get; set; }
+}
