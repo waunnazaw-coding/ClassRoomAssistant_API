bcea174de3d47779bb22a37efd20bb327dea43aa
waunnazaw-coding <yeminhtet3118@gmail.com>
2025-05-04 18:06:14 +0630

    feat : add new model for submissions for assignement

---
+++ Models/AssignmentSubmission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassRoomClone_App.Server.Models;
+
+public partial class AssignmentSubmission
+{
+    public int Id { get; set; }
+
+    public int AssignmentId { get; set; }
+
+    public int StudentId { get; set; }
+
+    public DateTime SubmittedAt { get; set; }
+
+    public virtual Assignment Assignment { get; set; } = null!;
+
+    public virtual User Student { get; set; } = null!;
+
+    public virtual ICollection<SubmissionResponse> SubmissionResponses { get; set; } = new List<SubmissionResponse>();
+}
---
+++ Models/SubmissionResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassRoomClone_App.Server.Models;
+
+public partial class SubmissionResponse
+{
+    public int Id { get; set; }
+
+    public int SubmissionId { get; set; }
+
+    public string ResponseType { get; set; } = null!;
+
+    public string? FilePath { get; set; }
+
+    public string? FileName { get; set; }
+
+    public long? FileSize { get; set; }
+
+    public string? MimeType { get; set; }
+
+    public string? Link { get; set; }
+
+    public DateTime UploadedAt { get; set; }
+
+    public virtual AssignmentSubmission Submission { get; set; } = null!;
+}
