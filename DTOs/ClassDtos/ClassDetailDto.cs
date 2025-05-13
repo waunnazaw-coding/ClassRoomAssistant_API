edaf1007a60afa44644fad61944021d622bf66aa
waunnazaw-coding <yeminhtet3118@gmail.com>
2025-05-13 15:57:05 +0630

    fix : fix response

---
+++ DTOs/ClassDtos/ClassDetailDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassRoomClone_App.Server.DTOs;
+
+public class ClassDetailDto
+{
+    public int EntityId { get; set; }
+    public string EntityType { get; set; }       
+    public string Content { get; set; }          
+    public DateTime ActivityDate { get; set; }
+    public int? MessageId { get; set; }
+    public int? SenderId { get; set; }
+    public int? ReceiverId { get; set; }
+    public string MessageContent { get; set; }   
+    public DateTime? MessageCreatedAt { get; set; }
+}
---
+++ DTOs/ClassDtos/GetClassDetailsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassRoomClone_App.Server.DTOs;
+
+public class GetClassDetailsResponse
+{
+    public List<ClassDetailDto> Details { get; set; } = new List<ClassDetailDto>();
+}
